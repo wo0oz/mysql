194ce1917738a156c05d9ec4e60218a5b8c3068b
wo0oz <128976011+wo0oz@users.noreply.github.com>
2023-11-18 11:35:04 +0900

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express"
+import mysql from "mysql"
+
+const app=express()
+
+const db=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"wjy0910wjy",
+    database:"book"
+});
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.json("hello this is the backend")
+});
+
+app.get("/books",(req,res)=>{
+    const q="SELECT * FROM books"
+    db.query(q,(err,data)=>{
+     if(err) return res.json(err)
+     return res.json(data)   
+    });
+});
+app.post("/books",(req,res)=>{
+    const q="INSERT INTO books ('title','desc','cover') VALUES (?)"
+    const values =[
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+        
+    ];
+
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Book has been created successly");
+    });
+});
+app.listen(8800,()=>{
+    console.log("연결되었습니다!")
+});
