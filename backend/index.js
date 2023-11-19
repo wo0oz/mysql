93982b2fcc680dfe062fdd08def6c2262e9b6e4a
wo0oz <128976011+wo0oz@users.noreply.github.com>
2023-11-19 17:53:34 +0900

    client

--- backend/index.js
+++ backend/index.js
@@ -10,33 +10,39 @@ const db=mysql.createConnection({
     database:"book"
 });
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.json("hello this is the backend")
 });
 
-app.get("/books",(req,res)=>{
-    const q="SELECT * FROM books"
-    db.query(q,(err,data)=>{
-     if(err) return res.json(err)
-     return res.json(data)   
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      return res.json(data);
     });
-});
-app.post("/books",(req,res)=>{
-    const q="INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)"
-    const values =[
-        req.body.title,
-        req.body.desc,
-        req.body.cover,
-    ];
+  });
 
-    db.query(q,[values],(err,data)=>{
-        if(err) return res.json(err);
-        return res.json("Book has been created successfully");
-    });
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
 });
 app.listen(8800,()=>{
     console.log("연결되었습니다!")
